684453886354219e47b2e8c7c7908162bb54c4ef
Robert Pruden <rdp1070@rit.edu>
2018-04-07 08:47:42 -0400

    The beginnings of a shader
    http://www.battlemaze.com/?p=279

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public bool isTalking;
+    public bool canMove;
+    public string state;
+    public GameObject Player { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
+        isTalking = false;
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isTalking == true)
+        {
+            canMove = false;
+        }
+
+    }
+}
