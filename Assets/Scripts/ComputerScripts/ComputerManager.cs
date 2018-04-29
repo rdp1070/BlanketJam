6e52f1241f43b11b0808cd6adec47f701d3db80d
bwalruth <bwalruth@secondavenuelearning.com>
2018-04-28 21:34:17 -0400

    Email Scene added.

---
+++ Assets/Scripts/ComputerScripts/ComputerManager.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerManager : MonoBehaviour
+{
+
+    public enum ComputerState
+    {
+        DESKTOP,
+        EMAIL,
+        BROWSER,
+        GAME
+    };
+
+    [System.Serializable]
+    public struct emailData
+    {
+        public string subject;
+        public string message;
+        public string reply;
+    }
+
+    public ComputerState activeState;
+    public List<Button> desktopButtons = new List<Button>();
+    public List<emailData> emails = new List<emailData>();
+    public Dictionary<string, emailData> activeEmails = new Dictionary<string, emailData>();
+    public List<EmailObject> emailObjects = new List<EmailObject>();
+    public List<Button> emailButtons = new List<Button>();
+    public List<Text> emailSubjects = new List<Text>();
+    int activeEmailIndex;
+
+    bool emailReplyActive = false;
+
+    public GameObject errorMessageDisplay;
+
+    public GameObject EmailDisplay;
+    public Vector2 emailObjectOffset;
+    public Vector2 emailSpaceBuffer;
+    public Text messageTextField;
+    public Text replyTextField;
+    public Button replyButton;
+    public Button sendButton;
+    public bool canExitEmail = true;
+    public int numEmails;
+
+    int replyIndex;
+
+    public bool emailActive;
+    public bool gameActive;
+    public bool browserActive;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (activeState)
+        {
+            case ComputerState.EMAIL:
+                if (emailReplyActive)
+                {
+                    if (Input.anyKeyDown)
+                    {
+                        if (replyIndex < emailObjects[activeEmailIndex].replyText.Length)
+                        {
+                            replyTextField.text += emailObjects[activeEmailIndex].replyText[replyIndex];
+                            replyIndex++;
+                        }
+                        else
+                        {
+                            //end message, enable reply button.
+                            sendButton.interactable = true;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    public void ChangeState(string state)
+    {
+        switch (state)
+        {
+            case "Email":
+                if (emailActive)
+                {
+                    activeState = ComputerState.EMAIL;
+                    EmailDisplay.SetActive(true);
+                    canExitEmail = true;
+                    emailObjects.Clear();
+                    activeEmails.Clear();
+                    for (int i = 0; i < numEmails; i++)
+                    {
+                        while (true)
+                        {
+
+                            int rand = Random.Range(0, emails.Count);
+                            if (!activeEmails.ContainsKey(emails[rand].message))
+                            {
+
+                                EmailObject e = new EmailObject();
+                                e.Init(emails[rand].subject, emails[rand].message, emails[rand].reply, i, this);
+                                emailObjects.Add(e);
+                                emailSubjects[i].text = emails[rand].subject;
+                                emailButtons[i].gameObject.SetActive(true);
+                                activeEmails.Add(emails[rand].subject, emails[rand]);
+                                break;
+                            }
+
+
+                        }
+                    }
+                }
+                else
+                {
+                    errorMessageDisplay.SetActive(true);
+                    for (int i = 0; i < desktopButtons.Count; i++)
+                    {
+                        desktopButtons[i].interactable = false;
+                    }
+                }
+                break;
+            case "Desktop":
+                if (activeState == ComputerState.EMAIL)
+                {
+                    if (canExitEmail)
+                    {
+                        activeState = ComputerState.DESKTOP;
+                        EmailDisplay.SetActive(false);
+                    }
+                    else
+                    {
+                        //trigger unfinished text.
+                    }
+                }
+                else
+                {
+                    activeState = ComputerState.DESKTOP;
+                }
+                break;
+            case "Game":
+                if (gameActive)
+                {
+                    activeState = ComputerState.GAME;
+                }
+                else
+                {
+                    errorMessageDisplay.SetActive(true);
+                    for(int i = 0; i < desktopButtons.Count; i++)
+                    {
+                        desktopButtons[i].interactable = false;
+                    }
+                }
+                break;
+            case "Browser":
+                if (browserActive)
+                {
+                    activeState = ComputerState.BROWSER;
+                }
+                else
+                {
+                    errorMessageDisplay.SetActive(true);
+                    for (int i = 0; i < desktopButtons.Count; i++)
+                    {
+                        desktopButtons[i].interactable = false;
+                    }
+                }
+                break;
+            case "Exit":
+                //leave scene.
+                break;
+        }
+    }
+
+    public void StartReply()
+    {
+        emailReplyActive = true;
+        replyIndex = 0;
+        replyTextField.gameObject.SetActive(true);
+        replyTextField.text = "";
+        replyButton.interactable = false;
+        replyButton.gameObject.SetActive(false);
+        sendButton.gameObject.SetActive(true);
+        sendButton.interactable = false;
+        canExitEmail = false;
+
+    }
+
+    public void SetActiveMessage(int i)
+    {
+        if (!emailReplyActive)
+        {
+            activeEmailIndex = i;
+            messageTextField.text = emailObjects[activeEmailIndex].message;
+
+            replyButton.interactable = !emailObjects[activeEmailIndex].replied;
+            replyButton.gameObject.SetActive(!emailObjects[activeEmailIndex].replied);
+            replyTextField.text = "";
+        }
+    }
+
+    public void SendMessage()
+    {
+        sendButton.interactable = false;
+        sendButton.gameObject.SetActive(false);
+        //replyButton.gameObject.SetActive(true);
+        emailObjects[activeEmailIndex].replied = true;
+        emailReplyActive = false;
+
+        for (int i = 0; i < emailObjects.Count; i++)
+        {
+            if (!emailObjects[i].replied)
+                return;
+        }
+
+        canExitEmail = true;
+    }
+
+    public void CloseErrorMessage()
+    {
+
+        errorMessageDisplay.SetActive(false);
+        for (int i = 0; i < desktopButtons.Count; i++)
+        {
+            desktopButtons[i].interactable = true;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ComputerScripts/EmailObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmailObject{
+
+
+    public string subject;
+    public string message;
+    public string replyText;
+    public bool replied = false;
+    int index;
+
+    private ComputerManager owner;
+
+    public void Init(string sub, string mes, string rep, int i, ComputerManager o)
+    {
+        subject = sub;
+        message = mes;
+        replyText = rep;
+        index = i;
+        owner = o;
+    }
+}
