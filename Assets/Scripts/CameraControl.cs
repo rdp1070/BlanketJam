8e96fff77ab71bc786b7f6f59d37c837197d96b2
Robert Pruden <rdp1070@rit.edu>
2018-04-30 21:36:16 -0400

    Use Spine Animations and write custom player controller.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
     public GameObject player;
-	
+    
 	// Update is called once per frame
 	void Update () {
-        if (player != null)
-            transform.SetPositionAndRotation(new Vector3(player.transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        //if (player != null)
+        //    transform.SetPositionAndRotation(new Vector3(player.transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,72 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine;
 using UnityStandardAssets._2D;
+using Spine.Unity;
 
-public class PlayerController : PlatformerCharacter2D
+public class PlayerController : MonoBehaviour
 {
 
     public bool canMove = true;
     public bool isInteracting = false;
+    float x;
+    public float speed = 5;
+    public Rigidbody2D rb;
+    public Transform transform;
+    public SkeletonAnimation skeletonAnimation;
+    private string currentAnimationName;
 
     private void Interact(InteractableObjectController interactableObject)
     {
-        //interactableObject.interacting = true;
-        //isInteracting = true;
-        //choiceDialogBox.SetChoices(currenInteractable.choices);
-        //choiceDialogBox.EnableTextBox();
         Debug.Log("You're doing it fam, you're interacting.");
-        // do all the stuff for the interacting.
-        // this should be different for each object.
     }
 
     private void EndInteract(InteractableObjectController interactableObject)
     {
-        //interactableObject.interacting = false;
-        //isInteracting = false;
+
         Debug.Log("You have done what you came to do.");
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        //var interactableObject = other.GetComponentInParent<InteractableObjectController>();
-        //if (interactableObject != null)
-        //{
-        //    Debug.Log("Object Entered the InteractableObjectCollider for " + interactableObject.display_name);
-        //    interactableObject.Display_Interaction_text(true);
-        //    currenInteractable = interactableObject;
-        //}
+
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        //var interactableObject = other.GetComponentInParent<InteractableObjectController>();
-        //if (interactableObject != null)
-        //{
-        //    Debug.Log("Object Exited the InteractableObjectCollider for " + interactableObject.display_name);
-        //    interactableObject.Display_Interaction_text(false);
-        //    currenInteractable = null;
-        //}
+
     }
 
     private void Update()
     {
+        x = Input.GetAxis("Horizontal") * speed;
+        rb = GetComponent<Rigidbody2D>();
 
-        //if (Input.GetButtonUp("Action") && isInteracting != true && currenInteractable != null)
-        //{
-        //    Debug.Log("Pressed Action while display Interaction is true");
-        //    //do stuff if the the display_text is showing
-        //    Interact(currenInteractable);
-        //}
-
-        if (canMove == false || isInteracting == true)
+        if (x > 0)
         {
-            Debug.Log("can't move rn");
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            SetAnimation("Walk_test");
+        }
+        else if (x < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            SetAnimation("Walk_test");
         }
         else {
-            //choiceDialogBox.choices = null;
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            SetAnimation("Idle");
         }
+    }
+
+    private void SetAnimation(string name) {
+
+        if (name == currentAnimationName)
+            return;
+
+        skeletonAnimation.state.SetAnimation(0, name, true);
+        currentAnimationName = name;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(x, rb.velocity.y);
+        // you use these for physics
     }
 }
