0ec3b83b2f1eaf206473b2a902dbfc3ed8ad5057
Robert Pruden <rdp1070@rit.edu>
2018-05-03 22:01:21 -0400

    Finish Demo of Couch Chat

--- Assets/Scripts/CouchScripts/ConversationController.cs
+++ Assets/Scripts/CouchScripts/ConversationController.cs
@@ -3,91 +3,188 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConversationController : MonoBehaviour {
+public class ConversationController : MonoBehaviour
+{
 
 
     public List<string> DialogOptionsPositive = new List<string>();
     public List<string> DialogOptionsNegative = new List<string>();
     public List<Sprite> sprites = new List<Sprite>();
-    public string activeDialogOptionPositive;
-    public string activeDialogOptionNegative;
+    public string activeDialogOptionPositive { get; set; }
+    public string activeDialogOptionNegative { get; set; }
+    public Fungus.Flowchart flowchart;
+    public UnityEngine.UI.Image ReactionImageHolder;
+    private bool active = true;
     private Sprite activeDialogSprite;
     public string inputString = "";
     private int currentNumDialogs = -1;
     public int maxDialogs = 0;
-    
+    public int positivity = 0;
 
     // Use this for initialization
-    void Start () {
-        PickOptions();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        inputString += Input.inputString;
+    void Start()
+    {
+        if (flowchart == null)
+            flowchart = GetComponent<Fungus.Flowchart>();
+    }
 
-        if (CheckIfDone(inputString))
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (active == true)
         {
-            return;
-        }
-
-        inputString = CheckIfInOptions(inputString);
-
-        // handle clicking backspace
-        if (Input.GetKeyDown(KeyCode.Backspace)) {
-            
-            if (!string.IsNullOrEmpty(inputString) && inputString.Length >= 1)
+            inputString += Input.inputString;
+            if (CheckIfDone(inputString))
             {
-                if (inputString.Length == 1)
+                inputString = "";
+                return;
+            }
+
+            inputString = CheckIfInOptions(inputString);
+
+            // handle clicking backspace
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+
+                if (!string.IsNullOrEmpty(inputString))
                 {
-                    inputString = "";
+                    //Debug.Log("backspace");
+                    if (inputString.Length == 1)
+                    {
+                        inputString = "";
+                    }
+                    else
+                    {
+
+                        inputString = inputString.Remove(inputString.Length - 2); // backspace is a character...
+
+                    }
+
+                    flowchart.SetStringVariable("inputString", inputString);
                 }
-                else {
-                    inputString = inputString.Remove(inputString.Length - 2);
-                }
-                // Debug.Log(inputString);
+            }
+
+            // update the screen
+            if (flowchart != null)
+            {
+                flowchart.SetStringVariable("inputString", inputString);
             }
         }
     }
 
-
-    private string CheckIfInOptions(string inputString)
+    public string CheckStrangeCharacters(string option)
     {
-        if (!string.IsNullOrEmpty(inputString) && inputString.Length >= 1 && !Input.GetKeyDown(KeyCode.Backspace))
+        if (Input.anyKeyDown && !Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.Backspace) && string.IsNullOrEmpty(Input.inputString))
         {
-            if (!activeDialogOptionPositive.Contains(inputString) && !activeDialogOptionNegative.Contains(inputString))
+            if (option.Length > inputString.Length)
             {
-                Debug.Log("WRONG");
-                Debug.Log(inputString + " " + activeDialogOptionPositive + " or " + activeDialogOptionNegative);
-                if (inputString.Length == 1)
+                char special_character = option[inputString.Length];
+                Debug.Log(special_character);
+                Debug.Log(special_character.ToString());
+                if (char.IsPunctuation(special_character) == true)
                 {
-                    return "";
-                }
-                else
-                {
-                    return inputString.Remove(inputString.Length - 2);
+                    return special_character.ToString();
                 }
             }
         }
-        return inputString;
+
+        return "";
+    }
+
+    private string CheckIfInOptions(string inputString)
+    {
+        if (inputString != null && inputString != "" && !Input.GetKeyDown(KeyCode.Backspace)
+            && !Input.GetKeyDown(KeyCode.LeftShift) && !Input.GetKeyDown(KeyCode.RightShift))
+        {
+            if (inputString.Length >= 1)
+            {
+                var negative_substr = activeDialogOptionNegative;
+                if (activeDialogOptionNegative.Length > inputString.Length)
+                {
+                    negative_substr = activeDialogOptionNegative.Substring(0, inputString.Length);
+                }
+
+                var positive_substr = activeDialogOptionPositive;
+                if (activeDialogOptionPositive.Length > inputString.Length)
+                {
+                    positive_substr = activeDialogOptionPositive.Substring(0, inputString.Length);
+                }
+
+
+                inputString += CheckStrangeCharacters(activeDialogOptionNegative);
+                if (negative_substr.Contains(inputString))
+                {
+                    // set the variable in the flowchart
+                    if (flowchart != null)
+                    {
+                        // update the boxes on the screen
+                        flowchart.ExecuteIfHasBlock("Correct Negative Text");
+                    }
+                } // end if negative
+
+                inputString += CheckStrangeCharacters(activeDialogOptionPositive);
+                if (positive_substr.Contains(inputString))
+                {
+
+                    // set the variable in the flowchart
+                    if (flowchart != null)
+                    {
+                        // update the boxes on the screen
+                        flowchart.ExecuteIfHasBlock("Correct Positive Text");
+                    }
+                } // end if positive
+                if (!positive_substr.Contains(inputString) && !(negative_substr.Contains(inputString)))
+                {
+                    Debug.Log("WRONG");
+                    Debug.Log(inputString);
+                    flowchart.ExecuteIfHasBlock("Shake Blocks");
+                    if (inputString.Length == 1)
+                    {
+                        inputString = "";
+                    }
+                    else
+                    {
+                        inputString = inputString.Remove(inputString.Length - 1);
+                    }
+                }// end if wrong input
+            }
+            else
+            {
+                inputString = "";
+            }
+        } // end if not null
+
+        return inputString; // if none of those cases (somehow) return what they gave you
     }
 
     private bool CheckIfDone(string inputString)
     {
+
+        if (currentNumDialogs == maxDialogs)
+        {
+            // transition to the end state of the level.
+            if (positivity > 0)
+                flowchart.ExecuteIfHasBlock("Positive End Text");
+            else
+                flowchart.ExecuteIfHasBlock("Negative End Text");
+            return true;
+        }
+
         // if the inputString is exactly equal to the option, print out a success message.
         if (inputString.Equals(activeDialogOptionPositive, System.StringComparison.CurrentCultureIgnoreCase))
         {
             Debug.Log("Happy Go Lucky");
-            inputString = "";
-            PickOptions();
+            positivity++;
+            flowchart.ExecuteIfHasBlock("Next Group");
             // call the flowchart say function
             return true;
         }
         else if (inputString == activeDialogOptionNegative)
         {
             Debug.Log("Debby Downer");
-            inputString = "";
-            PickOptions();
+            positivity--;
+            flowchart.ExecuteIfHasBlock("Next Group");
             // call the flowchart say function
             return true;
         }
@@ -95,7 +192,22 @@ public class ConversationController : MonoBehaviour {
         return false;
     }
 
-    void PickOptions() {
+    public void ToggleActive() {
+        active = !active;
+    }
+
+    public void PickOptions()
+    {
+
+        if (currentNumDialogs == maxDialogs)
+        {
+            // transition to the end state of the level.
+            if (positivity > 0)
+                flowchart.ExecuteIfHasBlock("Positive End Text");
+            else
+                flowchart.ExecuteIfHasBlock("Negative End Text");
+            return;
+        }
 
         var x = UnityEngine.Random.Range(0, DialogOptionsPositive.Count);
         activeDialogOptionPositive = DialogOptionsPositive[x];
@@ -106,6 +218,10 @@ public class ConversationController : MonoBehaviour {
         DialogOptionsNegative.Remove(activeDialogOptionNegative);
 
         activeDialogSprite = sprites[UnityEngine.Random.Range(0, sprites.Count)];
+        ReactionImageHolder.sprite = activeDialogSprite;
+
+        flowchart.SetStringVariable("activeDialogOptionPositive", activeDialogOptionPositive);
+        flowchart.SetStringVariable("activeDialogOptionNegative", activeDialogOptionNegative);
 
         currentNumDialogs++;
     }
