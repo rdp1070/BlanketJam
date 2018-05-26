c418ffd80ce1f2141f04f9f4b90fd3200ff8117a
Robert Pruden <rdp1070@rit.edu>
2018-05-26 13:59:21 -0400

    Remove Fungus Examples

---
+++ Assets/SpeechCollector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechCollector : MonoBehaviour
+{
+
+    public List<Fungus.SayDialog> sayDialogs;
+    public Camera camera;
+    public float dialogMargin;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (camera == null)
+        {
+            camera = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if active dialog is off camera, move to just on camera.
+        foreach (Fungus.SayDialog dialog in sayDialogs)
+        {
+            if (dialog != null)
+            {
+                // if the character is off screen move the dialog box to the edge
+                if (dialog.SpeakingCharacter.transform.position.x < (Camera.main.transform.position.x - Camera.main.orthographicSize))
+                {
+                    // this is to the left
+                    dialog.transform.position = new Vector3(Camera.main.transform.position.x - Camera.main.orthographicSize + dialogMargin,
+                        dialog.transform.position.y);
+
+                }
+                // if the character is off screen move the dialog box to the edge
+                else if (dialog.SpeakingCharacter.transform.position.x > (Camera.main.transform.position.x + Camera.main.orthographicSize))
+                {
+                    // this is to the right
+                    dialog.transform.position = new Vector3(Camera.main.transform.position.x + Camera.main.orthographicSize - dialogMargin,
+                        dialog.transform.position.y);
+                }
+                // if the character is on screen move the dialog box to above them.
+                else
+                {
+                    // on screen with character, move to the character.
+                    dialog.transform.position =
+                        new Vector3(dialog.SpeakingCharacter.transform.position.x, dialog.transform.position.y + dialogMargin);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/WandererController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class WandererController : MonoBehaviour {
+
+    public bool canMove = true;
+    public bool isInteracting = false;
+    float x;
+    public float speed = 5;
+    public Rigidbody2D rb;
+    public Transform transform;
+    public SkeletonAnimation skeletonAnimation;
+    private string currentAnimationName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+    }
+
+    private void Update()
+    {
+
+        if (canMove == true)
+        {
+
+            x = Input.GetAxis("Horizontal") * speed;
+            rb = GetComponent<Rigidbody2D>();
+            if (x > 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                SetAnimation("Walk_test");
+            }
+            else if (x < 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                SetAnimation("Walk_test");
+            }
+            else
+            {
+                SetAnimation("Idle");
+            }
+        }
+        else
+        {
+            x = Input.GetAxis("Horizontal") * 0;
+            SetAnimation("Idle");
+        }
+    }
+
+    public void SetCanMove(bool movable)
+    {
+        canMove = movable;
+    }
+
+    private void SetAnimation(string name)
+    {
+
+        if (name == currentAnimationName)
+            return;
+
+        skeletonAnimation.state.SetAnimation(0, name, true);
+        currentAnimationName = name;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(x, rb.velocity.y);
+        // you use these for physics
+    }
+}
